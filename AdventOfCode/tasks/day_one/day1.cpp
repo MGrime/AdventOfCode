c1833dfe2ac11f5d01b7c5cecc48f221dd6197a7
MichaHuey <michael@hueygames.com>
2021-12-01 10:46:16 +0000

    Completed Day 1

--- AdventOfCode/tasks/day_one/day1.cpp
+++ AdventOfCode/tasks/day_one/day1.cpp
@@ -1,11 +1,119 @@
 #include "day1.hpp"
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
 
 namespace aoc
 {
+	const char* inputPath = "day_one.txt";
+
+	void d1PartOne()
+	{
+		// Open input text
+		std::ifstream file;
+
+		file.open(inputPath);
+		if (file.fail())
+		{
+			std::cout << "Failed to open input file!";
+			return;
+		}
+
+		std::string str;
+		std::vector<int> inputNumbers;
+
+		// Read lines
+		while (std::getline(file, str))
+		{
+			int inputNum = std::stoi(str);
+			inputNumbers.push_back(inputNum);
+		}
+
+		// Loop lines
+
+		// First number
+		int prevNum = inputNumbers.at(0);
+
+		// Number bigger
+		int numBigger = 0;
+
+		for (int i = 1; i < inputNumbers.size(); ++i)
+		{
+			// Get curr num
+			int currNum = inputNumbers.at(i);
+
+			// Check
+			if (currNum > prevNum)
+			{
+				numBigger++;
+			}
+
+			// Store this as previous
+			prevNum = currNum;
+		}
+
+		// Output
+		std::cout << "Part 1: Amount of measurements bigger: " << numBigger << '\n';
+	}
+
+	void d1PartTwo()
+	{
+		// Open input text
+		std::ifstream file;
+
+		file.open(inputPath);
+		if (file.fail())
+		{
+			std::cout << "Failed to open input file!";
+			return;
+		}
+
+		std::string str;
+		std::vector<int> inputNumbers;
+
+		// Read lines
+		while (std::getline(file, str))
+		{
+			int inputNum = std::stoi(str);
+			inputNumbers.push_back(inputNum);
+		}
+
+		// Loop
+		int numBigger = 0;
+
+		int prevSum = inputNumbers.at(0) + inputNumbers.at(1) + inputNumbers.at(2);
+
+		for (int i = 1; i < inputNumbers.size(); ++i)
+		{
+			int currSum = inputNumbers.at(i);
+			if (i + 1 < inputNumbers.size())
+			{
+				currSum += inputNumbers.at(i + 1);
+			}
+			if (i + 2 < inputNumbers.size())
+			{
+				currSum += inputNumbers.at(i + 2);
+			}
+
+			if (currSum > prevSum)
+			{
+				numBigger++;
+			}
+
+			prevSum = currSum;
+		}
+
+		// Output
+		std::cout << "Part 2: Amount of measurements bigger: " << numBigger << '\n';
+
+	}
+
 	void executeDay1()
 	{
-		std::cout << "Doing day 1 weee waa wooo\n";
+		d1PartOne();
+		d1PartTwo();
 	}
 }
